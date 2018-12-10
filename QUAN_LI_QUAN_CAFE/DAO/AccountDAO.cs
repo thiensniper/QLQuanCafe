f64d94a043b0c7b6f4a6abb5e5368924e702d630
thiensniper <nguyenminhthiensmart@gmail.com>
2018-12-10 13:40:38 +0700

    fix login

--- QUAN_LI_QUAN_CAFE/DAO/AccountDAO.cs
+++ QUAN_LI_QUAN_CAFE/DAO/AccountDAO.cs
@@ -30,7 +30,7 @@ namespace QUAN_LI_QUAN_CAFE.DAO
 
         public bool CheckAccountExistsByID(string username, string password)
         {
-            string query = "SELECT * FROM TAIKHOAN WHERE username = '" + username + "' and password = '" + password + "'";
+            string query = "SELECT * FROM TAIKHOAN WHERE Username = '" + username + "' and Password = '" + password + "'";
             DataTable data = DataProvider.Instance.excuteQuery(query);
             return data.Rows.Count > 0;
         }
